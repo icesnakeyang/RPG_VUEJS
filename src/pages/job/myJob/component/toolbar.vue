f7be076125dade4b972a5d5fb41fff8f7c3a2ee7
icesnakeyang <flow_ocean@yahoo.com>
2018-10-14 10:59:33 +0800

    ok

--- src/pages/job/myJob/component/toolbar.vue
+++ src/pages/job/myJob/component/toolbar.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="card">
-    <Button type="primary">Primary</Button>
-    <Button type="info" @click="onLog">log</Button>
-    <Button type="success">Success</Button>
-    <Button type="warning">Warning</Button>
-    <Button type="error">Error</Button>
+    <Badge :count="5" type="error">
+      <Button type="info" @click="onLog" class="gogo_badge">{{$t("job.tabLog")}}</Button>
+    </Badge>
+    <Badge :count="5" type="error">
+      <Button type="success" class="gogo_badge">{{$t("job.tabComplete")}}</Button>
+    </Badge>
+    <Badge :count="5" type="error">
+      <Button type="warning" class="gogo_badge">{{$t("job.tabStop")}}</Button>
+    </Badge>
+    <Badge :count="5" type="error">
+      <Button type="error" class="gogo_badge">{{$t("job.tabSpotlight")}}</Button>
+    </Badge>
   </div>
 </template>
 
@@ -14,9 +21,9 @@
     methods: {
       onLog() {
         this.$router.push({
-          name:'jobLogPage',
-          params:{
-            jobId:this.$route.params.jobId
+          name: 'jobLogPage',
+          params: {
+            jobId: this.$route.params.jobId
           }
         })
       }
@@ -27,5 +34,15 @@
 <style scoped>
   .card {
     margin: 20px;
+  }
+
+  .gogo_badge {
+    /*width: 42px;*/
+    /*height: 42px;*/
+    /*background: #eee;*/
+    /*border-radius: 6px;*/
+    /*display: inline-block;*/
+    /*font-size: 14px;*/
+    margin-left: 20px;
   }
 </style>
