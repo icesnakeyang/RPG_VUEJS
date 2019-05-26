bc2f90edfa1d0c22fb606c1a862b99e6bca542ef
icesnakeyang <flow_ocean@yahoo.com>
2019-05-26 18:17:16 +0800

    Revert "don't know why, the quill-editor control cannot resize the image. so i changed the module version, then it can support"
    This reverts commit c365dd55

---
+++ src/pages/account/myAccountPage.vue
@@ -0,0 +1,146 @@
+<template>
+  <div>
+      <Table border :columns="columns5" :data="data5">
+
+      </Table>
+
+  </div>
+</template>
+
+<script>
+  import {apiListMyAccount} from "../../api/api"
+  import {rpgFormat} from "../../common/rpgfun";
+
+  export default {
+    name: "myAccountPage",
+    data() {
+      return {
+        columns5: [
+          {
+            title: this.$t("user.account.createdTime"),
+            key: 'cratedTime',
+            render: (h, params) => {
+              return h('div', rpgFormat.formatTime(params.row.createdTime))
+            }
+          },
+          {
+            title: this.$t("user.account.amount"),
+            key: 'amount',
+            sortable: true
+          },
+          {
+            title: this.$t("user.account.type"),
+            key: 'type',
+            render:(h, params)=>{
+              return h('div', this.$t("user.account."+params.row.type))
+            }
+          }
+        ],
+        data5: [],
+        columns2: [
+          {
+            title: 'Name',
+            key: 'name'
+          },
+          {
+            title: 'Age',
+            key: 'age',
+            render: (h, params) => {
+
+              if (params.row.$isEdit) {
+
+                return h('input', {
+
+                  domProps: {
+                    value: params.row.age
+                  },
+                  on: {
+                    input: function (event) {
+                      params.row.age = event.target.value
+                    }
+                  }
+                });
+              } else  {
+
+                return h('div', params.row.age);
+              }
+            }
+          },
+          {
+            title: 'Address',
+            key: 'address'
+          },
+          {
+            title: 'Action',
+            key: 'action',
+            render: (h, params) => {
+              return h('Button', {
+                props: {
+                  type: 'text',
+                  size: 'small'
+                },
+                on: {
+                  click: () => {
+                    if (params.row.$isEdit) {
+                      this.handleSave(params.row)
+                    } else {
+                      this.handleEdit(params.row)
+                    }
+                  }
+                }
+              }, params.row.$isEdit ? '保存' : '编辑')
+            }
+          }
+        ],
+        data3: [
+          {
+            name: '哈哈',
+            age: 18,
+            address: '上海',
+            $isEdit: false
+          },
+          {
+            name: '啦啦',
+            age: 24,
+            address: '北京',
+            $isEdit: false
+          }
+        ],
+        keyword:''
+      }
+    },
+    computed: {
+      items() {
+        return "ok"
+      }
+    },
+    methods: {
+      loadAllData() {
+        apiListMyAccount({
+          pageIndex: 0,
+          pageSize: 20
+        }).then((response) => {
+          if (response.data.errorCode === 0) {
+            this.data5 = response.data.data.content
+          } else {
+            this.$Message.error(this.$t('syserr.' + response.data.errorCode))
+          }
+        })
+      },
+      handleEdit (row) {
+        this.$set(row, '$isEdit', true)
+
+      },
+      handleSave (row) {
+        this.$set(row, '$isEdit', false)
+      }
+    },
+    mounted() {
+      this.loadAllData()
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/task/taskEdit.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <Form :model="task" :label-width="200">
+      <FormItem v-show="errInput">
+        <Alert type="error" show-icon>{{errMsg}}</Alert>
+      </FormItem>
+      <FormItem :label="$t('task.title')">
+        <Input v-model="task.title"/>
+      </FormItem>
+      <FormItem :label="$t('task.code')">
+        <Input v-model="task.code"/>
+      </FormItem>
+      <FormItem :label="$t('task.detail')">
+        <quill-editor v-model="task.detail"
+                      :options="editorOption"
+        ></quill-editor>
+      </FormItem>
+      <FormItem :label="$t('task.price')">
+        <Input v-model="task.price"/>
+      </FormItem>
+      <FormItem :label="$t('task.days')">
+        <Input v-model="task.days"/>
+      </FormItem>
+      <FormItem v-show="!saving">
+        <Button type="primary" @click="clickUpdate">{{$t("task.update")}}</Button>
+      </FormItem>
+      <FormItem v-show="saving">
+        <template>
+          <!--<Spin size="small"></Spin>-->
+          <!--<Spin></Spin>-->
+          <Spin size="large"></Spin>
+        </template>
+      </FormItem>
+    </Form>
+  </div>
+</template>
+
+<script>
+  import {apiGetTaskDetailByTaskId} from "../../api/api";
+  import {apiUpdateTask} from "../../api/api";
+  import {quillEditor} from "vue-quill-editor";
+  import {ImageResize} from 'quill-image-resize-module'
+
+  export default {
+    name: "taskEdit",
+    components:{
+      quillEditor
+    },
+    data(){
+      return{
+        task:{},
+        errInput:false,
+        errMsg:false,
+        saving:false,
+        editorOption:{
+          modules:{
+            imageResize:true
+          }
+        }
+      }
+    },
+    methods: {
+      loadAllData() {
+        apiGetTaskDetailByTaskId({
+          taskId: this.$store.state.taskId
+        }).then((response) => {
+            this.task = response.data.data.task;
+          })
+      },
+      clickUpdate(){
+        apiUpdateTask({
+          taskId:this.$store.state.taskId,
+          title:this.task.title,
+          detail:this.task.detail,
+          days:this.task.days,
+          code:this.task.code,
+          price:this.task.price
+        }).then((response)=>{
+          if(response.data.errorCode===0){
+            this.$router.push({name:'taskDetail',
+              params:{
+                taskId:this.$route.params.taskId
+              }
+            })
+          }else {
+            this.errInput=true;
+            this.errMsg=this.$t("task.err1")
+          }
+        })
+
+      }
+    },
+    created() {
+      this.loadAllData()
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
