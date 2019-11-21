fbc25f09976849d8e1559cb49a8b8c9e1d13b427
snake <flow_ocean@yahoo.com>
2019-11-21 19:26:54 +0800

    progress

---
+++ src/pages/layout/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="layout">
+    <Layout>
+      <Header style="padding: 0px">
+        <router-view name="head"></router-view>
+      </Header>
+      <Layout>
+        <Sider hide-trigger>
+          <router-view name="sider"></router-view>
+        </Sider>
+        <Layout :style="{padding: '0 24px 24px'}">
+          <router-view name="content"></router-view>
+        </Layout>
+      </Layout>
+      <router-view name="footer"></router-view>
+    </Layout>
+  </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                isCollapsed: false
+            }
+        },
+        computed: {
+            rotateIcon() {
+                return [
+                    'menu-icon',
+                    this.isCollapsed ? 'rotate-icon' : ''
+                ];
+            },
+            menuitemClasses() {
+                return [
+                    'menu-item',
+                    this.isCollapsed ? 'collapsed-menu' : ''
+                ]
+            }
+        },
+        methods: {
+            collapsedSider() {
+                this.$refs.side1.toggleCollapse();
+            }
+        }
+    }
+</script>
