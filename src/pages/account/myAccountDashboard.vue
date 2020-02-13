ce0c9b96320ada6f430c26266d5469fb5ed4d678
icesnakeyang <flow_ocean@yahoo.com>
2020-02-13 20:38:37 +0800

    progress

--- src/pages/account/myAccountDashboard.vue
+++ src/pages/account/myAccountDashboard.vue
@@ -4,51 +4,46 @@
       <BreadcrumbItem>{{$t('navigator.myAccount')}}</BreadcrumbItem>
     </Breadcrumb>
     <Content class="gogo_content">
-      <div class="ivu-row" style="margin-left: -10px; margin-right: -10px;">
-        <div class="ivu-col ivu-col-span-xs-12 ivu-col-span-md-8 ivu-col-span-lg-4"
-             style="height: 120px; padding-bottom: 10px; padding-left: 10px; padding-right: 10px;">
-          <div class="info-card-wrapper ivu-card ivu-card-dis-hover ivu-card-shadow"><!----> <!---->
-            <div class="ivu-card-body" style="padding: 0px;">
-              <div class="content-con">
-                <div class="left-area" style="background: rgb(45, 140, 240); width: 36%;">
-                  <i class="icon ivu-icon ivu-icon-logo-usd" style="font-size: 36px; color: rgb(255, 255, 255);"></i>
-                </div>
-                <div class="right-area" style="width: 64%;">
-                  <div>
-                    <div class="count-to-wrapper"><p class="content-outer"><span id="count_to_319"
-                                                                                 class="count-to-count-text count-style">{{balance}}</span><i
-                      class="count-to-unit-text"></i></p></div>
-                    <p>{{$t('user.account.balance')}}</p></div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="ivu-col ivu-col-span-xs-12 ivu-col-span-md-8 ivu-col-span-lg-4"
-             style="height: 120px; padding-bottom: 10px; padding-left: 10px; padding-right: 10px;">
-          <div class="info-card-wrapper ivu-card ivu-card-dis-hover ivu-card-shadow"><!----> <!---->
-            <div class="ivu-card-body" style="padding: 0px;">
-              <div class="content-con">
-                <div class="left-area" style="background: rgb(219,78,43); width: 36%;">
+      <Row>
+        <Col :xs="24" :sm="12" :md="12" :lg="4">
+          <Card style="margin: 10px;">
+            <p slot="title" class="title_text">我的余额</p>
+            <Form>
+              <FormItem>
+                <div class="label_text">账户可用余额</div>
+                <div class="money_text">199.99</div>
+              </FormItem>
+              <FormItem>
+                <div class="label_text">账户不可用余额</div>
+                <div class="money_none_text">0.00</div>
+              </FormItem>
+              <FormItem>
+                <Button type="primary">withdraw</Button>
+              </FormItem>
+            </Form>
+          </Card>
+        </Col>
+        <Col :xs="24" :sm="12" :md="12" :lg="20">
+          <Card style="margin: 10px">
+            <Row>
+              <Col :xs="24" :sm="24" :md="12" :lg="12">
+                <div style="background: rgb(219,78,43); width: 36%;">
                   <i class="icon ivu-icon ivu-icon-md-trending-up"
-                     style="font-size: 36px; color: rgb(255, 255, 255);"></i></div>
+                     style="font-size: 36px; color: rgb(255, 255, 255);"></i>
+                </div>
                 <div class="right-area" style="width: 64%;">
                   <div>
-                    <div class="count-to-wrapper"><p class="content-outer"><span id="count_to_325"
-                                                                                 class="count-to-count-text count-style">{{income}}</span><i
-                      class="count-to-unit-text"></i></p></div>
-                    <p>{{$t('user.account.income')}}</p></div>
+                    <div class="count-to-wrapper">
+                      <p class="content-outer">
+                        <span id="count_to_325" class="count-to-count-text count-style">{{income}}</span>
+                        <i class="count-to-unit-text"></i>
+                      </p>
+                    </div>
+                    <p>{{$t('user.account.income')}}</p>
+                  </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="ivu-col ivu-col-span-xs-12 ivu-col-span-md-8 ivu-col-span-lg-4"
-             style="height: 120px; padding-bottom: 10px; padding-left: 10px; padding-right: 10px;">
-          <div class="info-card-wrapper ivu-card ivu-card-dis-hover ivu-card-shadow"><!----> <!---->
-            <div class="ivu-card-body" style="padding: 0px;">
-              <div class="content-con">
-                <!--<div class="left-area" style="background: rgb(255, 153, 0); width: 36%;">-->
+              </Col>
+              <Col :xs="24" :sm="24" :md="12" :lg="12">
                 <div class="left-area" style="background: rgb(89,187,115); width: 36%;">
                   <i class="icon ivu-icon ivu-icon-md-trending-down"
                      style="font-size: 36px; color: rgb(255, 255, 255);"></i>
@@ -60,46 +55,65 @@
                       class="count-to-unit-text"></i></p></div>
                     <p>{{$t('user.account.outgoing')}}</p></div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+      </Row>
     </Content>
   </div>
 </template>
 
 <script>
-    import {apiLoadAccountBalance} from "../../api/api";
+  import {apiLoadAccountBalance} from "../../api/api";
 
-    export default {
-        name: "myAccountDashboard",
-        data() {
-            return {
-                income: 0,
-                outgoing: 0,
-                balance: 0
-            }
-        },
-        methods: {
-            loadAllData() {
-                apiLoadAccountBalance({}).then((response) => {
-                    if (response.data.errorCode === 0) {
-                        this.income = response.data.data.income
-                        this.outgoing = response.data.data.outgoing
-                        this.balance = response.data.data.balance
-                    } else {
-                        this.$Message.error(this.$t('syserr.' + response.data.errorCode))
-                    }
-                })
-            }
-        },
-        mounted() {
-            this.loadAllData()
-        }
+  export default {
+    name: "myAccountDashboard",
+    data() {
+      return {
+        income: 0,
+        outgoing: 0,
+        balance: 0
+      }
+    },
+    methods: {
+      loadAllData() {
+        apiLoadAccountBalance({}).then((response) => {
+          if (response.data.errorCode === 0) {
+            this.income = response.data.data.income
+            this.outgoing = response.data.data.outgoing
+            this.balance = response.data.data.balance
+          } else {
+            this.$Message.error(this.$t('syserr.' + response.data.errorCode))
+          }
+        })
+      }
+    },
+    mounted() {
+      this.loadAllData()
     }
+  }
 </script>
 
 <style scoped>
   @import '../../assets/gogoStyles.css';
+  .title_text{
+    font-size: 18px;
+    color: #353535;
+  }
+
+  .label_text{
+    font-size: 14px;
+    color: #9ca3b4;
+  }
+
+  .money_text{
+    font-size: 14px;
+    color: #009a14;
+  }
+
+  .money_none_text{
+    font-size: 14px;
+    color: #ea6963;
+  }
 </style>
