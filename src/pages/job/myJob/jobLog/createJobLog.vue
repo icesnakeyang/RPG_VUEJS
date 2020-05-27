d7f2a1c4e6423dc55c810e82199f58e6bc53283b
liuyang <flow_ocean@yahoo.com>
2020-05-27 17:05:28 +0800

    progress

--- src/pages/job/myJob/jobLog/createJobLog.vue
+++ src/pages/job/myJob/jobLog/createJobLog.vue
@@ -9,9 +9,7 @@
           <Alert type="error" show-icon>{{errMsg}}</Alert>
         </FormItem>
         <FormItem :label="$t('job.log.content')">
-          <Input type="textarea" v-model="jobLog.content"
-                 :placeholder="$t('job.log.contentHolder')"
-                 :autosize="{minRows: 5,maxRows: 15}"/>
+          <quill-editor v-model="jobLog.content"></quill-editor>
         </FormItem>
 
         <FormItem v-show="!saving">
@@ -30,45 +28,49 @@
 </template>
 
 <script>
-    import {apiCreateLog} from "../../../../api/api";
+  import {apiCreateLog} from "../../../../api/api";
+  import {quillEditor} from "vue-quill-editor";
 
-    export default {
-        name: "createJobLog",
-        data() {
-            return {
-                jobLog: {},
-                errInput: false,
-                errMsg: '',
-                saving: false
-            }
-        },
-        methods: {
-            onCreateLog() {
-                if (!this.$store.state.jobId) {
-                    return
-                }
-                if (!this.jobLog.content) {
-                    return;
-                }
-                const params={
-                    jobId: this.$store.state.jobId,
-                    content: this.jobLog.content
-                }
-                apiCreateLog(params).then((response) => {
-                    if (response.data.errorCode === 0) {
-                        this.$router.push({
-                            name: 'jobLogPage',
-                            params: {
-                                jobId: this.$store.state.jobId
-                            }
-                        })
-                    } else {
-                        this.$Message.error(this.$t('syserr.' + response.data.errorCode))
-                    }
-                })
-            }
+  export default {
+    name: "createJobLog",
+    components: {
+      quillEditor
+    },
+    data() {
+      return {
+        jobLog: {},
+        errInput: false,
+        errMsg: '',
+        saving: false
+      }
+    },
+    methods: {
+      onCreateLog() {
+        if (!this.$store.state.jobId) {
+          return
         }
+        if (!this.jobLog.content) {
+          return;
+        }
+        const params = {
+          jobId: this.$store.state.jobId,
+          content: this.jobLog.content
+        }
+        apiCreateLog(params).then((response) => {
+          if (response.data.errorCode === 0) {
+            this.$router.push({
+              name: 'jobLogPage',
+              params: {
+                jobId: this.$store.state.jobId
+              }
+            })
+          } else {
+            this.$Message.error(this.$t('syserr.' + response.data.errorCode))
+          }
+        })
+      }
     }
+  }
 </script>
 
 <style scoped>
--- src/pages/job/myJob/jobLog/jobLogRow.vue
+++ src/pages/job/myJob/jobLog/jobLogRow.vue
@@ -6,10 +6,10 @@
       </p>
       <p>{{$t("job.log.createdTime")}}: {{createdTime}}</p>
       <p v-if="readTime">{{$t("job.log.readTime")}}: {{readTime}}</p>
-      <p v-else="readTime">{{$t("job.log.readTime")}}: <Tag color="error">{{unRead}}</Tag></p>
-      <Input type="textarea" v-model="log.content"
-             :autosize="{minRows: 5,maxRows: 15}"
-      />
+      <p v-else="readTime">{{$t("job.log.readTime")}}:
+        <Tag color="error">{{unRead}}</Tag>
+      </p>
+      <quill-editor v-model="log.content" :options="options"></quill-editor>
     </Card>
   </div>
 </template>
@@ -17,24 +17,38 @@
 <script>
 
   import moment from "moment";
+  import {quillEditor} from "vue-quill-editor";
 
   export default {
     name: "jobLogRow",
-    props:{
-      log:{}
+    components: {
+      quillEditor
     },
-    computed:{
-      createdTime(){
+    data() {
+      return {
+        options: {
+          modules: {
+            toolbar: false
+          },
+          readOnly: true
+        },
+      }
+    },
+    props: {
+      log: {}
+    },
+    computed: {
+      createdTime() {
         return moment(this.log.createdTime).format('YYYY-MM-DD HH:mm')
       },
-      readTime(){
-        if(this.log.readTime){
+      readTime() {
+        if (this.log.readTime) {
           return moment(this.log.readTime).format('YYYY-MM-DD HH:mm')
-        }else{
+        } else {
           return false
         }
       },
-      unRead(){
+      unRead() {
         return this.$t('common.unRead')
       }
     }
@@ -42,7 +56,7 @@
 </script>
 
 <style scoped>
-  .card{
+  .card {
     margin: 20px;
   }
 </style>
